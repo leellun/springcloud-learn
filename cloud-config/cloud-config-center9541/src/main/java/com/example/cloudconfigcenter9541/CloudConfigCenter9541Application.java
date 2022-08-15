5e5c32e16325177aa11d1a552efc0b1cc1f8b238
leellun <leellun@sina.cn>
2022-08-15 17:53:24 +0800

    config-cloud

---
+++ cloud-config/cloud-config-center9541/src/main/java/com/example/cloudconfigcenter9541/CloudConfigCenter9541Application.java
@@ -0,0 +1,13 @@
+package com.example.cloudconfigcenter9541;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigCenter9541Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigCenter9541Application.class, args);
+    }
+
+}
---
+++ cloud-config/cloud-config-center9541/src/test/java/com/example/cloudconfigcenter9541/CloudConfigCenter9541ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudconfigcenter9541;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigCenter9541ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
