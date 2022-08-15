47958f16397ccc30a2f61363c540bbadec820447
leellun <leellun@sina.cn>
2022-08-15 21:41:33 +0800

    配置中心

--- cloud-config/config-client9542/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ cloud-config/config-client9542/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -1,28 +1,12 @@
 package com.example.configclient;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@EnableDiscoveryClient
 @EnableEurekaClient
-@RestController
-@RefreshScope
+@SpringBootApplication
 public class ConfigClientApplication {
-
-	@Value("${server.port}")
-	String foo;
-
-	@RequestMapping(value = "/hi")
-	public String hi() {
-		return foo;
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
