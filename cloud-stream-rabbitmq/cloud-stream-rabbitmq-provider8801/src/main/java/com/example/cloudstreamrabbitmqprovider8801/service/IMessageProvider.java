4d4f4f9716bec4a776306debfded3e827f665769
leellun <leellun@sina.cn>
2022-08-15 23:20:03 +0800

    cloud-stream-rabbitmq

---
+++ cloud-stream-rabbitmq/cloud-stream-rabbitmq-provider8801/src/main/java/com/example/cloudstreamrabbitmqprovider8801/service/IMessageProvider.java
@@ -0,0 +1,9 @@
+package com.example.cloudstreamrabbitmqprovider8801.service;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-22 10:55
+ */
+public interface IMessageProvider {
+    public String send();
+}
