7159a287b66337903387b050ac83faf9734ea42d
leellun <leellun@sina.cn>
2022-08-16 15:31:33 +0800

    alibaba nacos

---
+++ cloud-alibaba-nacos/cloudalibaba-consumer-nacos-order83/src/main/java/com/example/cloudalibabaconsumernacosorder83/CloudalibabaConsumerNacosOrder83Application.java
@@ -0,0 +1,39 @@
+package com.example.cloudalibabaconsumernacosorder83;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class CloudalibabaConsumerNacosOrder83Application {
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+    @Resource
+    private RestTemplate restTemplate;
+    @Value("${service-url.nacos-user-service}")
+    private String serverURL;
+
+    @GetMapping(value = "/consumer/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(serverURL + "/payment/nacos/" + id, String.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudalibabaConsumerNacosOrder83Application.class, args);
+    }
+
+}
