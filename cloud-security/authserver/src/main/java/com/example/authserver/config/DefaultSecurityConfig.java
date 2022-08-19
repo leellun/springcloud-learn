1d0749da06144109106a815dba733765acc95998
leellun <leellun@sina.cn>
2022-08-19 08:23:44 +0800

    354345345

---
+++ cloud-security/authserver/src/main/java/com/example/authserver/config/DefaultSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.authserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * Author: leell
+ * Date: 2022/8/18 23:58:06
+ */
+@EnableWebSecurity
+public class DefaultSecurityConfig {
+
+    // @formatter:off
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                ).formLogin(withDefaults());
+        return http.build();
+    }
+    // @formatter:on
+
+    // @formatter:off
+    @Bean
+    UserDetailsService users() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user1")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    // @formatter:on
+
+}
