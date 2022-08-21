c5e8a8d89f465ac3b205ce5ae8b4f3983699e9a0
leellun <leellun@sina.cn>
2022-08-21 23:51:59 +0800

    cloud-dubbo

--- cloud-dubbo/order-service/src/main/java/com/newland/order/SpringOrderApplication.java
+++ cloud-dubbo/order-service/src/main/java/com/newland/order/SpringOrderApplication.java
@@ -1,11 +1,9 @@
 package com.newland.order;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubbo
 public class SpringOrderApplication {
     public static void main(String[] args){
         SpringApplication.run(SpringOrderApplication.class);
