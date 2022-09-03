acc7c23cc3a974321492c493785b827dbd7d7966
leellun <leellun@sina.cn>
2022-09-03 12:06:51 +0800

    单点登录

---
+++ cloud-sso/client1/src/main/java/com/newland/client1/controller/Client1Controller.java
@@ -0,0 +1,30 @@
+package com.newland.client1.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Client1Controller {
+
+    @GetMapping("/high")
+    @PreAuthorize("hasAuthority('ROLE_HIGH')")
+    public String normal( ) {
+        return "high permission";
+    }
+
+    @GetMapping("/mid")
+    @PreAuthorize("hasAuthority('ROLE_MID')")
+    public String medium() {
+        return "mid permission";
+    }
+
+    @GetMapping("/low")
+    @PreAuthorize("hasAuthority('ROLE_LOW')")
+    public String admin() {
+        return "low permission";
+    }
+
+}
---
+++ cloud-sso/spring-security-oauth/src/test/java/com/newland/oauth/SpringSecurityOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newland.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
