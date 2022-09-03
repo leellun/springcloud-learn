2e14ef79f6b92da5f47dba2f8da6fcb1b58f5e67
leellun <leellun@sina.cn>
2022-09-04 00:26:15 +0800

    单点登录

--- cloud-security/education2/src/main/java/com/example/education2/security/config/WebSecurityConfig.java
+++ cloud-security/education2/src/main/java/com/example/education2/security/config/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.education2.security.config;
 
+import com.example.education2.filter.AuthenticationFilter;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * 资源服务配置
@@ -13,14 +16,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public AuthenticationFilter authenticationFilter(){
+        return new AuthenticationFilter();
+    }
     /**
      * 请求配置
      */
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http
+        http.addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .antMatcher("/**").authorizeRequests()
                 .anyRequest().authenticated();
     }
