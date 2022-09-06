90c5e3712c9f0ad08d7328d7d4186f623c48c781
leellun <leellun@sina.cn>
2022-09-07 00:56:22 +0800

    王改吗下面

--- cloud-alibaba-seata/seata-order-service2001/src/main/java/com/example/seataorderservice2001/SeataOrderService2001Application.java
+++ cloud-alibaba-seata/seata-order-service2001/src/main/java/com/example/seataorderservice2001/SeataOrderService2001Application.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableTransactionManagement
 @SpringBootApplication(exclude =  DataSourceAutoConfiguration.class) //取消数据源的自动创建
 public class SeataOrderService2001Application {
 
